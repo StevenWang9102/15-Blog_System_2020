152fef7d34568b66cf48a81f53aa6ff9fb078671
Sibo Wang <375973811@qq.com>
2020-01-13 16:50:15 +1300

    Globle Feed implement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import './App.css';
 import Nav from './Components/Nav.js'
+import { loadInitialData } from "./ReduxStore/FeedDetails/feedActions";
+
 
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-function App() {
+const App = (props) => {
+  useEffect(() => {
+    props.loadInitialData();
+  }, [props]);
+
   return (
       <div className="App">
         <Nav/>
@@ -13,4 +20,16 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = ({ saveBookStore, usersBooksChanged, setStudentName }) => {
+  return { saveBookStore, usersBooksChanged, setStudentName };
+}; //@@@@@@@@@@@@@此处不对劲
+
+const mapDismatchToProps = dispatch => {
+  return {
+    loadInitialData: () => {
+      dispatch(loadInitialData());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDismatchToProps)(App);
--- src/Components/NavComponent/MainPage.js
+++ src/Components/NavComponent/MainPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+import ArticlePreview from '../Article_preview'
+import PopularTages from '../PopularTags'
+
 
 const MainPage = props => {
+
+  console.log(props.articleLibrary);
+  
   return (
     <div>
-    
       <div class='home-page'>
         <div class='banner'>
           <div class='container'>
@@ -11,110 +17,20 @@ const MainPage = props => {
             <p>A place to share your knowledge.</p>
           </div>
         </div>
-
-        <div class='container page'>
-          <div class='row'>
-            <div class='col-md-9'>
-              <div class='feed-toggle'>
-                <ul class='nav nav-pills outline-active'>
-                  <li class='nav-item'>
-                    <a class='nav-link disabled' href='https://www.baidu.com/'>
-                      Your Feed
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link active' href='https://www.baidu.com/'>
-                      Global Feed
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <div class='article-preview'>
-                <div class='article-meta'>
-                  <a href='profile.html'>
-                    <img src='http://i.imgur.com/Qr71crq.jpg' alt=''/>
-                  </a>
-                  <div class='info'>
-                    <a href='https://www.baidu.com/' class='author'>
-                      Eric Simons
-                    </a>
-                    <span class='date'>January 20th</span>
-                  </div>
-                  <button class='btn btn-outline-primary btn-sm pull-xs-right'>
-                    <i class='ion-heart'></i> 29
-                  </button>
-                </div>
-                <a href='https://www.baidu.com/' class='preview-link'>
-                  <h1>How to build webapps that scale</h1>
-                  <p>This is the description for the post.</p>
-                  <span>Read more...</span>
-                </a>
-              </div>
-
-              <div class='article-preview'>
-                <div class='article-meta'>
-                  <a href='profile.html'>
-                    <img src='http://i.imgur.com/N4VcUeJ.jpg' alt="img"/>
-                  </a>
-                  <div class='info'>
-                    <a href='https://www.baidu.com/' class='author'>
-                      Albert Pai
-                    </a>
-                    <span class='date'>January 20th</span>
-                  </div>
-                  <button class='btn btn-outline-primary btn-sm pull-xs-right'>
-                    <i class='ion-heart'></i> 32
-                  </button>
-                </div>
-                <a href='https://www.baidu.com/' class='preview-link'>
-                  <h1>
-                    The song you won't ever stop singing. No matter how hard you
-                    try.
-                  </h1>
-                  <p>This is the description for the post.</p>
-                  <span>Read more...</span>
-                </a>
-              </div>
-            </div>
-
-            <div class='col-md-3'>
-              <div class='sidebar'>
-                <p>Popular Tags</p>
-
-                <div class='tag-list'>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    programming
-                  </a>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    javascript
-                  </a>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    emberjs
-                  </a>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    angularjs
-                  </a>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    react
-                  </a>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    mean
-                  </a>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    node
-                  </a>
-                  <a href='https://www.baidu.com/' class='tag-pill tag-default'>
-                    rails
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
+      <ArticlePreview/>
+      <PopularTages/>
     </div>
   );
 };
 
-export default MainPage;
+const mapStateToProps = (state) => {
+  return { ...state}; // 暂时@@@
+}; 
+
+
+const mapDismatchToProps = dispatch => {
+  return {
+  };
+};
+export default connect(mapStateToProps, mapDismatchToProps)(MainPage);
