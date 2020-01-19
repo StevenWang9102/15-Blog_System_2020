40c3e44571c9e00358dbb833de5bfce3aadd1c59
Sibo Wang <375973811@qq.com>
2020-01-19 15:33:20 +1300

    -

---
+++ src/Components/MainPageComponent/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = props => {
+  return (
+    <div class='home-page'>
+      <div class='banner'>
+        <div class='container'>
+          <h1 class='logo-font'>conduit</h1>
+          <p>A place to share your knowledge.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/ReduxStore/store.js
@@ -0,0 +1,9 @@
+import { feedReducer } from "./FeedDetails/feedReducers";
+import { feedSaga } from "./FeedDetails/feedSagas";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(feedReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(feedSaga);
