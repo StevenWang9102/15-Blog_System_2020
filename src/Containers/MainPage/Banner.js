e9b1e955261afa93206bea8d79db548c1bd1adaa
Sibo Wang <375973811@qq.com>
2020-03-31 10:34:30 +1300

    mock up api

--- src/Containers/MainPage/Banner.js
+++ src/Containers/MainPage/Banner.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export const Banner = props => {
   return (
-    <div className="home-page">
-      <div className="banner">
-        <div className="container">
-          <h1 className="logo-font">conduit</h1>
+    <div className='home-page'>
+      <div className='banner'>
+        <div className='container'>
+          <h1 className='logo-font'>conduit - {process.env.REACT_APP_TITLE}</h1>
           <p>A place to share your knowledge.</p>
         </div>
       </div>
     </div>
   );
-};
+}
